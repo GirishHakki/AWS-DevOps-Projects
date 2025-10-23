bbbd8556b96622399b1c5d0a360a691d794aee9a
GirishHakki <109576032+GirishHakki@users.noreply.github.com>
2025-10-23 16:35:47 +0530

    Add files via upload

---
+++ 3tier-app-deployment-aws/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.c9u8ckmqq2v5.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin123456',
+    DB_DATABASE : 'webappdb'
+});
